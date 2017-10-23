99e1fb0a62ec31b8c03a22cd198d599d8f3dd568
Katie Maxwell <katie@codeinstitute.net>
2017-10-23 09:02:28 +0000

    add person function added

--- teams.py
+++ teams.py
@@ -4,15 +4,27 @@ def show_menu():
     print("3. Stats")
     print("4. Exit")
 
-    option = input("Enter option: ")
+    option = input("Enter option:\n> ")
     return option
 
+def add_person():
+    first_name = input("Enter First Name: ")
+    last_name = input("Enter Last Name: ")
+    age = input("Enter Age: ")
+    team = input("Enter Team: ")
+    
+    line = first_name + "," + last_name + "," + age + "," + team + "\n"
+    
+    print(line)
+    
+    with open("options.txt", "a") as people_file:
+        people_file.write(line)
 
 def person_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You selected 'Add Person'")
+            add_person()
         elif option == "2":
             print("You selected 'View People'")
         elif option == "3":
