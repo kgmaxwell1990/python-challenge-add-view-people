a48beba97d05ebae95e6e9f9222570a8d0301560
Katie Maxwell <katie@codeinstitute.net>
2017-10-23 09:36:03 +0000

    view stats function

--- teams.py
+++ teams.py
@@ -21,7 +21,7 @@ def add_person():
         people_file.write(line)
    
         
-def view_people():
+def load_people():
     with open("options.txt", "r") as people_file:
         people = []
         
@@ -30,12 +30,41 @@ def view_people():
         for line in lines:
             fields = line.split(",")
             people.append(fields)
-        
+    return people
+
+def view_people():
+    people = load_people()
     print("List of People")
     
     for person in people:
         print("First Name: {0}, Last Name: {1}, Age: {2}, Team: {3}".format(person[0], person[1], person[2], person[3], ))
 
+def view_stats():
+    people = load_people()
+    
+    red_team = []
+    blue_team = []
+    other = []
+    
+    total_age = 0
+    
+    for person in people:
+        total_age += int(person[2])
+        if person[3] == "red":
+            red_team.append(person)
+        elif person[3] == "blue":
+            blue_team.append(person)
+        else:
+            other.append(person)
+        
+    average_age = total_age / len(people)
+    
+    print("The number of people is {0}".format(len(people)))
+    print("The average age is {0}".format(average_age))
+    
+    print(red_team)
+    print(blue_team)
+    print(other)
 
 def person_loop():
     while True:
@@ -45,7 +74,7 @@ def person_loop():
         elif option == "2":
             view_people()
         elif option == "3":
-            print("You selected 'Stats'")
+            view_stats()
         elif option == "4":
             break
         else:
